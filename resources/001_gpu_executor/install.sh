bd46b0881ba09ccaaeed86b2aae76824a72ee22f
avidalto <alvaro@parallelworks.com>
2024-06-04 17:25:08 +0000

    fix bugs

--- resources/001_gpu_executor/install.sh
+++ resources/001_gpu_executor/install.sh
@@ -1,7 +1,7 @@
 
-mkdir -p ${workdir}/pw/bootstrap/
+mkdir -p ${resource_workdir}/pw/bootstrap/
 
-miniconda_install_dir=${workdir}/pw/bootstrap/miniconda3
+miniconda_install_dir=${resource_workdir}/pw/bootstrap/miniconda3
 conda_repo="https://repo.anaconda.com/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh"
 
 # Check if the directory exists
@@ -17,18 +17,16 @@ source ${miniconda_install_dir}/etc/profile.d/conda.sh
 conda activate ldm
 if [ $? -ne 0 ]; then
     echo "Installing Stable Diffussion for GPUs"
-    cd ${workdir}/pw/bootstrap/
+    cd ${resource_workdir}/pw/bootstrap/
     git clone https://github.com/CompVis/stable-diffusion.git
     cd stable-diffusion/
     conda env create -f environment.yaml
     conda activate ldm
 fi
 
-if ! [ -f ${workdir}/pw/bootstrap/stable-diffusion/sd-v1-4.ckpt ]; then
-    cd ${workdir}/pw/bootstrap/stable-diffusion/
+if ! [ -f ${resource_workdir}/pw/bootstrap/stable-diffusion/sd-v1-4.ckpt ]; then
+    cd ${resource_workdir}/pw/bootstrap/stable-diffusion/
     curl https://f004.backblazeb2.com/file/aai-blog-files/sd-v1-4.ckpt > sd-v1-4.ckpt
 fi
-
-export txt2image_script="${workdir}/pw/bootstrap/stable-diffusion/scripts/txt2img.py"
 
 cd ${resource_jobdir}
--- resources/001_gpu_executor/txt2img.sh
+++ resources/001_gpu_executor/txt2img.sh
@@ -1,2 +1,3 @@
 
-python3 ${workdir}/pw/bootstrap/stable-diffusion/scripts/txt2img.py --prompt "sd_prompt" --plms --ckpt sd-v1-4.ckpt --skip_grid --n_samples ${sd_n_samples}
+cd ${resource_workdir}/pw/bootstrap/stable-diffusion/
+python3 ${resource_workdir}/pw/bootstrap/stable-diffusion/scripts/txt2img.py --prompt "${sd_prompt}" --plms --ckpt sd-v1-4.ckpt --skip_grid --n_samples ${sd_n_samples} --outdir ${resource_jobdir}
